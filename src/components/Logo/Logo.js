9065415ac8d00b70569c30b64f31f0dbc1e75cd3
Priyanshu Singh <priyanshusingh.gh@outlook.com>
2022-09-24 17:25:18 +0530

    deploy by removing particle and tilt

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import Tilt from 'react-tilt';
+import Tilt from 'react-parallax-tilt';
 import brain from './brain.png';
 import './Logo.css';
 
 const Logo = () => {
   return (
-    <div className='ma4 mt0'>
-      <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
-        <div className="Tilt-inner pa3">
-          <img style={{paddingTop: '5px'}} alt='logo' src={brain}/>
+    <div className="ma4 mt0">
+      <Tilt
+        className="Tilt parallax-effect-glare-scale br2 shadow-2"
+        perspective={500}
+        gyroscope={true}
+        style={{ height: 150, width: 150 }}
+      >
+        <div className="Tilt-inner inner-element pa3">
+          <img style={{ paddingTop: '5px' }} alt="logo" src={brain} />
         </div>
       </Tilt>
     </div>
   );
-}
+};
 
 export default Logo;
